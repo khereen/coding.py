eddefde41520782e1f32b313737bce6301ab284c
sansan <khereen@hotmail.com>
2019-12-03 15:21:41 +0800

    add commit

--- r.py
+++ r.py
@@ -1,13 +1,17 @@
 import random
 
-r = random.randint(1,100)
+r = random.randint(1,10)
+count = 0
 while True:
+	count +=1 #count = count +1
 	num = input('guess no.:')
 	num = int(num)
 	if num == r:
 		print('well done')
+		print('this is your', count , 'chance')
 		break
 	elif num > r:
 		print('more than')
 	elif num < r:
 		print('less than')
+	print('this is your', count , 'chance')
